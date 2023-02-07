52b867e9b4c2df7cc3c35d4c27a93962e03f6d41
unknown <acsgyorgy.mail@gmail.com>
2023-02-07 16:37:57 +0100

    server is working

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+let cors = require("cors");
+let fileSystem = require("fs");
+
+const app = express();
+const path = require("path");
+
+app.use(express.static("assets"));
+app.use(cors());
+app.use(express.json());
+app.set("port", 4000);
+
+app.get("/", (request,response) => {
+    const responseObject = {
+        text: "server is working on port!"
+    }
+    response.end(JSON.stringify(responseObject));
+});
+
+const server = app.listen(app.get("port"), function() {
+
+    let host = server.address().address
+    let port = server.address().port
+
+    console.log("app listening:", port)
+})
