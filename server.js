5695cf2e315b1848972ed33d2a5b31f90550028d
unknown <acsgyorgy.mail@gmail.com>
2023-02-17 21:14:25 +0100

    successful data retrieval from the mysql database

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ app.use(cors());
 app.use(express.json());
 app.set("port", 4000);
 
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "bookmania",
+  password: "1234",
+  database: "bookmania"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query(`select book.id, title, number_of_page, genre.genre, publisher.publisher_name , author.author_name, price, image, newness from book
+  inner join author on author.id = book.author
+  inner join genre on genre.id = book.genre
+  inner join publisher on publisher.id = book.publisher`, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
 app.get("/", (request,response) => {
     const responseObject = {
         text: "server is working on port!"
@@ -25,6 +45,6 @@ const server = app.listen(app.get("port"), function() {
     console.log("app listening:", port)
 })
 
-app.get("/img/bookread.jpg", function (req, res) {
-    res.sendFile(path.join(__dirname, "img/bookread.jpg"));
+app.get("/img/:filename", function (req, res) {
+    res.sendFile(path.join(__dirname, "img/" + req.params.filename));
 })
