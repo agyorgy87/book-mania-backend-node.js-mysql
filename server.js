e76dd82a010d2184f9a499029ae8677a5ce7777a
unknown <acsgyorgy.mail@gmail.com>
2024-08-01 09:22:53 +0200

    created new db on freedb. env file fixed.

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static("assets"));
 app.use(cors());
 app.use(express.json());
 app.set("port", process.env.PORT || 3001);
+//app.set("port", process.env.PORT || 3000);
 
 const mysql = require('mysql2');
 const { error } = require("console");
